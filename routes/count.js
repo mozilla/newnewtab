cc958effeafb5009d2f6d3f8cc7ebe69a1ae1ae3
Piotr Zalewa <piotr@zalewa.info>
2012-06-05 14:30:44 +0200

    webApp is not hosted on marketplace

--- routes/count.js
+++ routes/count.js
@@ -14,11 +14,10 @@ module.exports = function(app, dbClient) {
   app.post('/clicked_to/:slug', function(req, res) {
     // send click info to the back-end and redirect to provided URL
     // :attribute: slug (string) string identification of the webApp 
-    // :post: wabAppUrl (string) required, URL if the webApp in the 
-    //        marketplace
+    // :post: wabAppUrl (string) required, full URL of the webApp
     marketplace.addClick(dbClient, req.params.slug, function() {
       console.log('[debug] redirecting to ' + req.body.webAppUrl);
-      res.redirect(conf.get('marketplaceAPIDomain') + req.body.wabAppUrl);
+      res.redirect(req.body.wabAppUrl);
     });
   });
 }
